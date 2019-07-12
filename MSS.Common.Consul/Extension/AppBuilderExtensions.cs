d2ad3c7631e9193937519f681d6d3b2a283a0d8d
hyx <huyanxinag@outlook.com>
2019-07-12 13:24:51 +0800

    统一用一个配置

--- MSS.Common.Consul/Extension/AppBuilderExtensions.cs
+++ MSS.Common.Consul/Extension/AppBuilderExtensions.cs
@@ -1,20 +1,21 @@
 ﻿using Consul;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Options;
 using System;
 
 namespace MSS.Common.Consul
 {
     public static class AppBuilderExtensions
     {
-        public static IApplicationBuilder RegisterConsul(this IApplicationBuilder app, IApplicationLifetime lifetime, ServiceEntity serviceEntity)
+        public static IApplicationBuilder RegisterConsul(this IApplicationBuilder app, IApplicationLifetime lifetime,IOptions<ServiceEntity>  serviceEntity)
         {
-            var consulClient = new ConsulClient(x => x.Address = new Uri($"http://{serviceEntity.ConsulIP}:{serviceEntity.ConsulPort}"));//请求注册的 Consul 地址
+            var consulClient = new ConsulClient(x => x.Address = new Uri($"http://{serviceEntity.Value.ConsulIP}:{serviceEntity.Value.ConsulPort}"));
             var httpCheck = new AgentServiceCheck()
             {
                 DeregisterCriticalServiceAfter = TimeSpan.FromSeconds(5),//服务启动多久后注册
                 Interval = TimeSpan.FromSeconds(10),//健康检查时间间隔，或者称为心跳间隔
-                HTTP = $"http://{serviceEntity.IP}:{serviceEntity.Port}/health",//健康检查地址
+                HTTP = $"http://{serviceEntity.Value.IP}:{serviceEntity.Value.Port}/health",//健康检查地址
                 Timeout = TimeSpan.FromSeconds(5),
                 
              
@@ -25,11 +26,11 @@ namespace MSS.Common.Consul
             {
                 Checks = new[] { httpCheck },
                 ID = Guid.NewGuid().ToString(),
-                Name = serviceEntity.ServiceName,
-                Address = serviceEntity.IP,
-                Port = serviceEntity.Port,
+                Name = serviceEntity.Value.ServiceName,
+                Address = serviceEntity.Value.IP,
+                Port = serviceEntity.Value.Port,
                 
-                Tags = new[] { $"urlprefix-/{serviceEntity.ServiceName}" }//添加 urlprefix-/servicename 格式的 tag 标签，以便 Fabio 识别
+                Tags = new[] { $"urlprefix-/{serviceEntity.Value.ServiceName}" }//添加 urlprefix-/servicename 格式的 tag 标签，以便 Fabio 识别
             };
 
             consulClient.Agent.ServiceRegister(registration).Wait();//服务启动时注册，内部实现其实就是使用 Consul API 进行注册（HttpClient发起）
